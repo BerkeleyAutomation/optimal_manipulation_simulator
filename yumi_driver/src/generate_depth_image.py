d0c16b4c4655ea758cf985a14d8fcd72924771f2
chris_correa_interns <chris.correa.interns@cloudminds.com>
2017-03-22 17:33:39 -0700

    working on generating depth images

--- yumi_driver/src/generate_depth_image.py
+++ yumi_driver/src/generate_depth_image.py
@@ -1,13 +1,19 @@
-import meshpy
-import perception
+from meshpy import VirtualCamera, ObjFile, SceneObject
+from perception import CameraIntrinsics
+from core import RigidTransform
+import numpy as np
 dexnet_path = "/home/cloudminds/chomp_ws/src/dex-net-new-api"
 
 if __name__ == "__main__":
-    # camera_intr = CameraIntrinsics("world", fx=f, fy=f,
-    #                                cx=cx, cy=cy, skew=0.0,
-    #                                height=self.im_height, width=self.im_width)
-    # camera = meshpy.VirtualCamera(camera_intr)
-    camera = perception.VirtualKinect2Sensor()
-    of = meshpy.ObjFile(dexnet_path + "/data/meshes/chess_pieces/WizRook.obj")
+    camera_intr = CameraIntrinsics("world", fx=1000, height=480, width=360)
+    camera = VirtualCamera(camera_intr)
+    # camera = perception.VirtualKinect2Sensor()
+    of = ObjFile(dexnet_path + "/data/meshes/chess_pieces/WizRook.obj")
     mesh = of.read()
-    
+    # position = RigidTransform(from_frame="rook")
+    # scene_rook = SceneObject(mesh, position)
+    T_world_camera = RigidTransform(rotation = np.array([[-1,0,0],[0,1,0],[0,0,-1]]),
+                                    translation=np.array([0,0,1]).T,
+                                    from_frame="world",
+                                    to_frame="camera")
+    camera.images(mesh, [T_world_camera]);
